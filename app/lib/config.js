ac1ef7dd2fbfc3ea24a982fb851206e315923415
adafia <adafia.samuel@gmail.com>
2020-01-31 13:04:42 +0200

    create http and https server
     - add http and https server
     - add test route
    
    [Finishes create-http-https-server]

---
+++ app/lib/config.js
@@ -0,0 +1,33 @@
+/*
+ * Create and export configuration variables
+ *
+ */
+
+// openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+
+
+// container for all the environments
+const environments = {};
+
+// Staging (default) environment
+environments.staging = {
+    'httpPort': 3000,
+    'httpsPort': 3001,
+    'envName': 'staging'
+};
+
+// Production environment
+environments.production = {
+    'httpPort': 5000,
+    'httpsPort': 5001,
+    'envName': 'production'
+}
+
+// Determine which environment was passed as a command-line argument
+let currentEnvironment = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// check that the current environment is one of the environment above if not, default to staging
+let environmentToExport = typeof (environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// export the module
+module.exports = environmentToExport;
